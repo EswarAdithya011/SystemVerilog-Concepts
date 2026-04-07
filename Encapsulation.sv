a202b684b5a54bb4d4dbc12d4fe2b4f622dc34e4
Korrapolu Eswar Adithya <141947460+EswarAdithya011@users.noreply.github.com>
2026-04-08 00:25:40 +0530

    Update Encapsulation.sv

--- Encapsulation.sv
+++ Encapsulation.sv
@@ -1,45 +1,45 @@
-// 						ENCAPSULATION
-// 						LOCAL ACCESS MODIFIER
+						ENCAPSULATION
+						LOCAL ACCESS MODIFIER
 
-// module tb;
+module tb;
 
-//   class parent;
-//     local int a;
+  class parent;
+    local int a;
 
-//     function new(int x);
-//       a = x;
-//     endfunction
+    function new(int x);
+      a = x;
+    endfunction
 
-//     function void show();
-//       $display("Inside parent, a = %0d", a);
-//     endfunction
-//   endclass
-
-
-//   class child extends parent;
-
-//     function new(int x);
-//       super.new(x);
-//     endfunction
-
-//     function void access_parent();
-//       $display("Trying to access a = %0d", a);  // SHOULD FAIL
-//     endfunction
-
-//   endclass
+    function void show();
+      $display("Inside parent, a = %0d", a);
+    endfunction
+  endclass
 
 
-//   parent p;
-//   child  c;
+  class child extends parent;
 
-//   initial begin
-//     p = new(10);
-//     c = new(20);
+    function new(int x);
+      super.new(x);
+    endfunction
 
-//     p.show();     // Works
+    function void access_parent();
+      $display("Trying to access a = %0d", a);  // SHOULD FAIL
+    endfunction
 
-//     p.a = 5;      // SHOULD FAIL (module access)
-//     c.access_parent();  //  SHOULD FAIL (child access)
-//   end
+  endclass
 
-// endmodule
+
+  parent p;
+  child  c;
+
+  initial begin
+    p = new(10);
+    c = new(20);
+
+    p.show();     // Works
+
+    p.a = 5;      // SHOULD FAIL (module access)
+    c.access_parent();  //  SHOULD FAIL (child access)
+  end
+
+endmodule
