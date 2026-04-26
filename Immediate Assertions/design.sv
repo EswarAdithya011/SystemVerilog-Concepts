3ea9f2b91bc3550e0706ba800193d80d32c03090
Korrapolu Eswar Adithya <141947460+EswarAdithya011@users.noreply.github.com>
2026-04-26 16:14:39 +0530

    Create design.sv

---
+++ Immediate Assertions/design.sv
@@ -0,0 +1,19 @@
+
+
+// module grant(
+//   input logic clk, rst, req,
+//   output logic gnt);
+  
+//   logic gnt_d1;
+//   always_ff @(posedge clk) begin
+//     if (rst) begin
+//       gnt <= 0;
+//       gnt_d1 <= 0;
+//     end
+//     else begin
+//         gnt_d1 <= req;
+//         gnt <= gnt_d1;
+//     end        
+//   end
+  
+// endmodule
